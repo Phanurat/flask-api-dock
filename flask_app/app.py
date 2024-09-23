8a4a446b129cf2f53a2a1ea3c2f8d3e9c135e4c9
Panurat <paunavt5@gmail.com>
2024-09-23 13:24:59 +0700

    re -re -re

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import mysql.connector
 
 app = Flask(__name__)
-#app.secret_key = 'your_secret_key'  # สำหรับ flash message
+app.secret_key = 'your_secret_key'  # สำหรับ flash message
 
 # ฟังก์ชันสำหรับเชื่อมต่อกับฐานข้อมูล
 def get_db_connection():
