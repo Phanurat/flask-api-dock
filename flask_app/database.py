3910ceeac1db1b2e5691d0af17bffe6fe98b8500
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-09-27 11:01:38 +0700

    add path db URL

--- flask_app/database.py
+++ flask_app/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database URL
-DATABASE_URL = "mysql+mysqlconnector://root:1111@db/iron"
+# deploy
+# DATABASE_URL = "mysql+mysqlconnector://root:1111@db/iron"  
+
+# local
+DATABASE_URL = "mysql+mysqlconnector://root@localhost/iron"  
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
